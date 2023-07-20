e4cdc14efd17ec58da8034427a31dc3f32d73c8b
Milka4eva <114588326+Milka4eva@users.noreply.github.com>
2023-07-20 10:39:39 +0000

    Deploying to gh-pages from @ Milka4eva/goit-react-hw-08-phonebook@6bab26b70508d7ff31a8ca3b2b495e27f4aa24bb 🚀

---
+++ static/js/415.618883fd.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[415],{5415:function(e,t,n){n.r(t);var i=n(3329),c={container:{minHeight:"calc(50vh - 50px)",minWidth:"calc(50vw - 50px)",display:"flex",alignItems:"center",justifyContent:"center",flexDirection:"column"},title:{fontWeight:700,fontSize:30,textAlign:"center"}};t.default=function(){return(0,i.jsx)("div",{style:c.container,children:(0,i.jsx)("h1",{style:c.title,children:"Let's try this Phonebook App!"})})}}}]);
+//# sourceMappingURL=415.618883fd.chunk.js.map
